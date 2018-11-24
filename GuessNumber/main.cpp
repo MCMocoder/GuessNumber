7512230d935ec10a471a8fe9e48c4fd90bc8a2fe
Einstein Zheng <mocoder@icloud.com>
2018-11-24 17:18:59 +0800

    Initial Commit

---
+++ GuessNumber/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GuessNumber
+//
+//  Created by Zheng on 11/24/18.
+//  Copyright © 2018 Monkey Coder Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
